4f7ec1bfaea208f2dfba3bae6e4120f38e6c7ea5
Hasaranga <hasarangawije2345@gmail.com>
2022-11-06 00:25:06 +0530

    home push photo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Home.css"
 
 function Home() {
   return (
     <div>
-      <div>Home</div>
+      <div className="hero-image" style={{height:200}}>ss</div>
+      <div>Time table</div>
       <Link className="btn btn-info" to={"/view-bus"}>
         bus
       </Link>
